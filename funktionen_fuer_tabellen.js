4697b92c963a2e87168d6992be2e437c6003a1b9
MMatGitHub <mima-ffm@gmx.de>
2023-09-11 00:14:41 +0200

    Deltaberechnung

--- funktionen_fuer_tabellen.js
+++ funktionen_fuer_tabellen.js
@@ -10,6 +10,7 @@ function json2Table(json_daten, inWelcheTabelle){
       spalten.push(x);
     }
   }
+  spalten.push('delta');
 
   for (let ueberschrift of spalten){
     let th = document.createElement("th");
@@ -19,14 +20,37 @@ function json2Table(json_daten, inWelcheTabelle){
 
   for (let i = 0; i < json_daten.length; i++) {
     tabellenzeile = table.insertRow();
-
     for (let j = 0; j < spalten.length; j++) {
       let tabCell = tabellenzeile.insertCell(-1);
       tabCell.innerHTML = json_daten[i][spalten[j]];
+      if ( j === spalten.length-1){
+        try{
+          if (i===0){
+            tabCell.innerText = "";
+          }
+          let delta = Number(json_daten[i][spalten[j-2]]);
+          let deltaAlt = Number(json_daten[i-1][spalten[j-2]]);
+          let differenz = 0; 
+          differenz = Number(Math.round(deltaAlt-delta)); 
+          if (differenz >0){
+            tabCell.innerText = "+"+differenz;
+          } else {
+            tabCell.innerText = differenz;
+          }
+
+        }
+        catch (e) {
+          console.log(e.stack);   
+        }
+      }
     }
+   
+    
   }
+
   table.setAttribute("class",'mm-tabelle');
   document.getElementById('tabellendaten').appendChild(table);
+
 }
 
 console.log('funktionen_fuer_tabellen.js geladen');
