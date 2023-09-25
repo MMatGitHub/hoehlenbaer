4fc376d3b1c3220f2b195bbb09415dced68b6ce3
MMatGitHub <mima-ffm@gmx.de>
2023-09-26 01:16:56 +0200

    1 file modified mit rotem Punkt

--- main_site_home.js
+++ main_site_home.js
@@ -1,18 +1,17 @@
 class Home {
+  #privaterString
   constructor(derDefault) {
     this.derDefault = derDefault;
+     this.#privaterString='iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==';
   }
-  
-  ausfuehren() {
+ausfuehren() {
     jammer(this.derDefault);
-    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4
-  //8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==" alt="Red dot" />
     const dasBild = document.createElement("img");
+    dasBild.src='data:image/png;base64,'+this.#privaterString;
     
     dasBild.alt="Lecker Salat!";
+    document.getElementById('spielfeld').innerHTML="";
     document.getElementById('spielfeld').appendChild(dasBild);
-    //document.getElementById('spielfeld').innerHTML="";
-    //document.getElementById('spielfeld').innerHTML=dasBild;
   }
 
 }
