c4614a3349c1a2a29f9864a50c6d9c971d820a2f
Mi Ma <mima@sein.io>
2024-10-03 09:20:30 +0200

    lifes going with Jahressummen

--- funktionen_fuer_tabellen.js
+++ funktionen_fuer_tabellen.js
@@ -46,6 +46,9 @@ function json2Table(json_daten) {
   let mw2022result = [];
   let mw2023result = [];
   let mw2024result = [];
+  let jsumme2022 = 0;
+  let jsumme2023 = 0;
+  let jsumme2024 = 0;
   let DieWerte = [];
   
   for (let x of Object.keys(json_daten[0])) {
@@ -61,9 +64,11 @@ function json2Table(json_daten) {
   const Tage_spaltenname='Tage';
   const Delta_spaltenname='Delta';
   const Mittelwert_spaltenname='MW';
+  const Jahressumme_spaltenname='JSumme';
   spalten.push(Tage_spaltenname);
   spalten.push(Delta_spaltenname);
   spalten.push(Mittelwert_spaltenname);
+  spalten.push(Jahressumme_spaltenname);
 
   for (let ueberschrift of spalten) {
     let th = document.createElement('th');
@@ -111,6 +116,34 @@ function json2Table(json_daten) {
           tabCell.innerText=  berechneterWert;
         }
       }
+      //Jahressumme_spaltenname
+      if (spalten[j] === Jahressumme_spaltenname) {
+        let jsumme = Jahressummespaltebefuellen(deltaspalte[i]);   
+        let spalte_2=2;
+        let aktuellesJahr = String(
+          json_daten[i][spalten[spalte_2]]
+        );
+        tabCell.innerText = "-";
+        if (aktuellesJahr.includes("2022")){
+          if (!isNaN(jsumme)) {
+            jsumme2022 = jsumme2022+jsumme;
+            tabCell.innerText =jsumme2022
+          }
+        }
+        if (aktuellesJahr.includes("2023")){
+          if (!isNaN(jsumme)) {
+            jsumme2023 = jsumme2023+jsumme;
+            tabCell.innerText =jsumme2023
+          }
+        }
+        if (aktuellesJahr.includes("2024")){
+          if (!isNaN(jsumme)) {
+            jsumme2024 = jsumme2024+jsumme;
+            tabCell.innerText =jsumme2024
+          }
+        }
+      }
+
       //Mittelwert
       if (spalten[j] === Mittelwert_spaltenname) {
         let mittelwert = Mittelwertspaltebefuellen(deltaspalte[i],tagespalte[i]);
@@ -142,15 +175,18 @@ function json2Table(json_daten) {
   let w2024 = document.createElement("p");
   let wAll = document.createElement("p");
   let sumVonAnbeginn = document.createElement("p");
-  w2022.innerHTML="MW (2022): " +calculateMean(mw2022result);
-  w2023.innerHTML="MW (2023): " +calculateMean(mw2023result);
-  w2024.innerHTML="MW (2024): " +calculateMean(mw2024result);
+  w2022.innerHTML="MW (2022): " +calculateMean(mw2022result)+ ", SUM (2022): " +jsumme2022;
+  //jammer("hä:"  +calculateSum(mw2022result));
+  w2023.innerHTML="MW (2023): " +calculateMean(mw2023result)+ ", SUM (2023): " +jsumme2023;
+  w2024.innerHTML="MW (2024): " +calculateMean(mw2024result)+ ", SUM (2024): " +jsumme2024;
   wAll.innerHTML="MW (ges.): " +calculateMean(mwresult);
   let ersterWert = Number(DieWerte[0]);
+  if (ersterWert<1){let ersterWert = Number(DieWerte[1])};
+
   let letzerWert = Number(DieWerte[DieWerte.length-1]);
   let dieSumme = Number(letzerWert-ersterWert);
   dieSumme = dieSumme.toFixed(2);
-  sumVonAnbeginn.innerHTML="Gesamt: "+(dieSumme);
+  sumVonAnbeginn.innerHTML="Gesamt: "+(dieSumme) + ", abgerunde ges. für (2022-2024): "+(Number(jsumme2022)+Number(jsumme2023)+Number(jsumme2024));
   tabellenzeile.appendChild(w2022); 
   tabellenzeile.appendChild(w2023); 
   tabellenzeile.appendChild(w2024); 
@@ -173,7 +209,10 @@ function Mittelwertspaltebefuellen(zaehler, nenner){
   retVal = differenz;
   return retVal;
 }
-
+function Jahressummespaltebefuellen(einWert){
+  zahlwert= Number(einWert)
+  return Number(zahlwert);
+}
 function deltaspaltebefuellen(alt, neu){ 
     let differenz = 0;
     differenz = Number(Math.round(alt - neu));
@@ -189,6 +228,32 @@ function deltaspaltebefuellen(alt, neu){
 
 function timediffspaltebefuellen(alt, neu){
     return dateDifference(alt, neu);
+}
+
+function calculateSum(numbers) {
+  if (numbers.length === 0) {
+    return NaN; 
+  }
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    let einWert=Number(numbers[i]);
+    if (typeof einWert !== 'number' || isNaN(einWert) || !isFinite(einWert)) {
+      continue; // Skip non-numeric, NaN, or Infinity values
+    }
+    sum += einWert;
+  }
+  if (sum === 0) {
+    jammer("Nullsumme-keine Daten: Komisch!");
+    return 0;
+  }
+  if (sum < 0) {
+    jammer("Invertiere negative Summe");
+    sum = sum * -1;
+    zahl = Number(sum);
+
+    return zahl.toFixed(2);
+  }
+  return sum;
 }
 
 function calculateMean(numbers) {
